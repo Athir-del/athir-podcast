70cfd39f2efcf4279653045bfb7ab85af8ccf7b4
Ali Djezzar <agdjezzar@gmail.com>
2026-04-30 09:36:53 +0100

    fix music real tracks

--- public/music.js
+++ public/music.js
@@ -1,67 +1,20 @@
-class MusicPlayer {
-  constructor() {
-    this.ctx = null;
-    this.nodes = [];
-    this.playing = false;
-  }
+const tracks = {
+  calm: 'https://assets.mixkit.co/music/preview/mixkit-dreaming-big-31.mp3',
+  energetic: 'https://assets.mixkit.co/music/preview/mixkit-hip-hop-02-738.mp3',
+  tech: 'https://assets.mixkit.co/music/preview/mixkit-tech-house-vibes-130.mp3'
+};
 
-  init() {
-    this.ctx = new (window.AudioContext || window.webkitAudioContext)();
-  }
+let current = null;
 
-  playCalm() {
-    this.stop();
-    this.init();
-    const notes = [261, 294, 329, 349, 392];
-    notes.forEach((freq, i) => {
-      const osc = this.ctx.createOscillator();
-      const gain = this.ctx.createGain();
-      osc.frequency.value = freq;
-      osc.type = 'sine';
-      gain.gain.value = 0.05;
-      osc.connect(gain);
-      gain.connect(this.ctx.destination);
-      osc.start(this.ctx.currentTime + i * 0.5);
-      this.nodes.push(osc);
-    });
-    this.playing = true;
-  }
-
-  playEnergetic() {
-    this.stop();
-    this.init();
-    const osc = this.ctx.createOscillator();
-    const gain = this.ctx.createGain();
-    osc.frequency.value = 140;
-    osc.type = 'square';
-    gain.gain.value = 0.03;
-    osc.connect(gain);
-    gain.connect(this.ctx.destination);
-    osc.start();
-    this.nodes.push(osc);
-    this.playing = true;
-  }
-
-  playTech() {
-    this.stop();
-    this.init();
-    const osc = this.ctx.createOscillator();
-    const gain = this.ctx.createGain();
-    osc.frequency.value = 220;
-    osc.type = 'sawtooth';
-    gain.gain.value = 0.02;
-    osc.connect(gain);
-    gain.connect(this.ctx.destination);
-    osc.start();
-    this.nodes.push(osc);
-    this.playing = true;
-  }
-
+window.musicPlayer = {
+  play(type) {
+    if (current) { current.pause(); current = null; }
+    current = new Audio(tracks[type]);
+    current.loop = true;
+    current.volume = 0.3;
+    current.play();
+  },
   stop() {
-    this.nodes.forEach(n => { try { n.stop(); } catch(e) {} });
-    this.nodes = [];
-    this.playing = false;
+    if (current) { current.pause(); current = null; }
   }
-}
-
-window.musicPlayer = new MusicPlayer();
+};
