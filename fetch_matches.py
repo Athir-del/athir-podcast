7786d6e7e3da2ec445604edd35f23aea3773bb32
Ali Djezzar <ali.djezzar@gmail.com>
2026-05-06 02:17:00 +0100

    تحديث حي للمباريات

--- fetch_matches.py
+++ fetch_matches.py
@@ -12,7 +12,7 @@ def save_matches():
         if response.status_code == 200:
             data = response.json()
             # حفظ البيانات في ملف محلي
-            with open("matches.json", "w", encoding="utf-8") as f:
+            with open("public/matches.json", "w", encoding="utf-8") as f:
                 json.dump(data, f, ensure_ascii=False, indent=4)
             print("✅ تم حفظ البيانات في matches.json")
         else:
